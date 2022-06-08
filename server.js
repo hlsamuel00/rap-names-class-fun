ac8eaf2ccbe77bcc8c2bb083d67aafe4f3ee8f21
Harvey S <hlsamuel00@yahoo.com>
2022-06-08 13:16:42 -0400

    update port info for heroku

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('/api/:rapperName', (req,res)=>{
     }
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The Server is running on port ${PORT}! You better go catch it!!`)
 })
